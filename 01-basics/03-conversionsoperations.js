18572e90204eee7743e0e4309f3381c14acc8040
saman <samanrazzaq764@gmail.com>
2024-09-18 14:37:54 +0000

    conversion of data types

---
+++ 01-basics/03-conversionsoperations.js
@@ -0,0 +1,15 @@
+let score= "33hdgg"
+console.log(typeof score);
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber );
+console.log( valueInNumber );
+
+// "33" =33
+//"33abdj" = NaN which means not a number 
+// undefined = undefined 
+// null = 0
+// true = 1 ,false = 0
+ let IsLoggedIn = 1 
+ let booleanIsLoggedIn = Boolean(IsLoggedIn)
+ console.log(booleanIsLoggedIn);
+ // it shows that true which means 1 in boolean 
