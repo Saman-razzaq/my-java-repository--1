2abdfbb720cedb821bac918a3091c0bae432650a
saman <samanrazzaq764@gmail.com>
2024-09-18 09:45:46 +0000

    varriables and constant add

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,12 @@
+const accountId = 123123
+let accountemail = "saman@gmail.com"
+var accountpasword ="1122"
+accountcity ="lahore"
+
+//accountId = 45 not allowed
+accountemail="hellow@gmail.com"
+accountpasword="6666"
+accountcity="gujranwala"
+console.log(accountcity);
+console.table({accountId,accountemail,accountpasword,accountcity})
+/* do not use var it is not helpful now /*
