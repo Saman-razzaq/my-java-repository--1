4afabe7478fc904edf68a359f64678d150690200
saman <samanrazzaq764@gmail.com>
2024-09-18 14:07:41 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3)// we are using node js not browers this work in browser in node js it have another syntax
+console.log(3
+    +3
+)//code readability should be high its not a good practice yet
+console.log("saman" )
+
+let name ="sana "
+let age = 12
+let islogin= true
+// different datatypes :
+// number 
+//,string "" 
+//,boolean true/false ,
+//null it is stand alone value 
+//, undefined value na di ho koi bhi
+//symbol 
+//object
+//typeof to find any datatype 
+console .log (typeof age);
+console .log (typeof null); //object
+console .log (typeof undefined); //undefined
