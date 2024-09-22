7924746c438237fa09832aff9716e0af592ce4ff
saman <samanrazzaq764@gmail.com>
2024-09-22 14:19:16 +0000

    heap and stack

---
+++ 01-basics/06-stackHeap.js
@@ -0,0 +1,28 @@
+//primitive memory
+
+//7 types number,string,boolean ,null undefined,symbol,bigInt
+
+
+// referance (non primitive)
+
+
+//3 types array ,function(),object
+
+//const heros =[ "iqbal","shaheen" ];
+
+//let myobject={
+    //name :"saba"
+   // age:22
+}
+//const myFunction = function(){
+    //console.log("hello world");
+}
+//console.log(typeof  heros);
+//__________________________________________________________________
+           //memories
+           //1-stack memory(primitive type)
+           //2-heap memory(non primitive type )
+           let myYoutubename ="abcDotcom"
+           let anotherName = "myYoutubename"
+           anotherName ="chaiorcode"
+           console.log(anotherName);
