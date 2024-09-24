9f3a389300dfaed37ac0e09b217f40137f294031
saman <samanrazzaq764@gmail.com>
2024-09-24 09:13:08 +0000

    working on heap

--- 01-basics/06-stackHeap.js
+++ 01-basics/06-stackHeap.js
@@ -13,16 +13,27 @@
 //let myobject={
     //name :"saba"
    // age:22
-}
+//}
 //const myFunction = function(){
     //console.log("hello world");
-}
+//}
 //console.log(typeof  heros);
 //__________________________________________________________________
            //memories
            //1-stack memory(primitive type)
            //2-heap memory(non primitive type )
            let myYoutubename ="abcDotcom"
-           let anotherName = "myYoutubename"
+           let anotherName = myYoutubename
            anotherName ="chaiorcode"
            console.log(anotherName);
+          
+ let userOne = {
+            email:"abc@gamil.com",
+            upi:" user@hsdj"
+
+           }
+ let userTwo = userOne
+ userTwo.email = "saman@google.com"
+ console.log(userOne.email);
+ console.log(userTwo.email);
+ //stack mn copy or haep mn reference milta h 
