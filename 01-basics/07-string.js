2aa9f1530e433e6560d58d24decc9854b50b0600
saman <samanrazzaq764@gmail.com>
2024-09-27 15:56:15 +0000

    in process string

--- 01-basics/07-string.js
+++ 01-basics/07-string.js
@@ -1,3 +1,7 @@
-//const name ="saman "
-//const number = 50
-//console.log (`Hello ny name is ${name} my number is ${number}`);
+const name ="saman "
+const repoCount = 50
+
+console.log(name + repoCount);
+//console.log (`Hello ny name is ${name} my number is ${repoCount}`);
+const gameName = new String ( 'sana')
+console.log (gameName[0]);
