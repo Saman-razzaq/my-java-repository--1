6378e7cee7ab1296cca0023fdf0c01c56c190b01
saman <samanrazzaq764@gmail.com>
2024-10-09 09:36:09 +0000

    add maths

---
+++ 01-basics/09-maths.js
@@ -0,0 +1,18 @@
+console.log (Math);
+
+console.log (Math.round(277.7));
+
+console.log (Math.abs(-277.7));
+//abs change the sign from minus to plus but not change plus to minus 
+console.log (Math.sqrt(9));
+
+// squar root of any value 
+console.log (Math.ceil(9.6));
+//highest value 
+console.log (Math.floor(9.6));
+//to see the lowest value 
+console.log (Math.random());
+//it always give us a value between 0 and 1
+const =10
+const=20
+console.log (Math.floor(Math.random() * ( max - min + 1)) + min )
