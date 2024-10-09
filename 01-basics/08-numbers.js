60c292782143ac9904c487f6eff8509a00210517
saman <samanrazzaq764@gmail.com>
2024-10-09 08:35:43 +0000

    add numbers

---
+++ 01-basics/08-numbers.js
@@ -0,0 +1,12 @@
+const score=480
+console.log (score);
+const balance  = new Number(1000)
+console.log (balance);
+console.log (balance.toString().length.toExponential);
+console.log(typeof balance);
+const othernumber =37.7778
+console.log (othernumber.toPrecision(2));
+
+const array1 = [2, 3, 1];
+console.log(Math.min(...array1));
+console.log(Math.max(...array1));
