14c1f14d7f220a2bfc9b93da8b2b30ed4cd80009
saman <samanrazzaq764@gmail.com>
2024-10-16 17:01:03 +0000

    basics about functions

---
+++ 03-basics/01-functions.js
@@ -0,0 +1,23 @@
+function sayMyName(){
+    console.log("S");
+    console.log("A");
+    console.log("M");
+    console.log("A");
+    console.log("N");
+    
+}
+ sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+ function addTwoNumbers(number1, number2){
+
+   // let result = number1 + number2
+     //return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+console.log("Result: ", result);
