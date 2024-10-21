9675cb98fba4d34b04d0982516ba90794be79f87
saman <samanrazzaq764@gmail.com>
2024-10-21 14:21:58 +0000

    add interesting case

--- 03-basics/04-scopeLevels.js
+++ 03-basics/04-scopeLevels.js
@@ -17,5 +17,24 @@ function one(){ // one wala jo parent h wo child yani two ko acess nai kr skta j
 
      two()
 
+
 }
 one()
+//****************************interesting case************************************ */
+
+// two techniques of functions 
+
+// 1 // ye dono tareeqy sy work kry ga chahy hum function sy pehly likh lain ya function k bad bhi
+//console.log (addone(5));
+function addone (num) {
+    return num+1
+}
+//console.log (addone(5));
+
+//2// ye sirf tab kam kry ga jab is ko function k bad declear kia jay ga q k is mn variable  use kia gya h 
+//console.log (addtw0(5));
+const addtw0= function(num){
+    return  num+2
+}
+console.log (addtw0(5));
+ 
